c9b01c08084ad52ac34505f8059b870587703478
Gislain95 <ghislain.chakam@yahoo.fr>
2025-11-09 15:22:40 +0100

    Mise en place des formulaires réactifs et les régles de validations.

--- src/app/task-form/task-form.ts
+++ src/app/task-form/task-form.ts
@@ -1,72 +1,45 @@
 import { Component } from '@angular/core';
 import {BoldItalicPipe} from "../pipes/bold-italic-pipe";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {UpperCasePipe} from "@angular/common";
 import {TaskType} from '../types/task.type';
 
 @Component({
   selector: 'app-task-form',
-    imports: [
-        BoldItalicPipe,
-        FormsModule,
-        UpperCasePipe
-    ],
+  imports: [
+    BoldItalicPipe,
+    FormsModule,
+    UpperCasePipe,
+    ReactiveFormsModule
+  ],
   templateUrl: './task-form.html',
   styleUrl: './task-form.scss'
 })
 export class TaskForm {
 
-  protected enterTitleTask = "";
-  protected enterDescriptionTask= "";
   protected myTasks: TaskType [] = [];
   private currentTask: TaskType | null = null;
-  protected validationMessage = "";
 
 
-  protected titleWasEmpty = true;
-  protected descriptionWasEmpty = true;
-
-
-  public onEditTitleTask(titleTask: string): void {
-    this.titleWasEmpty = titleTask.trim().length < 0;
-  }
-
-
-  public onEditDescriptionTask(descriptionTask: string): void {
-    this.descriptionWasEmpty = descriptionTask.trim().length < 0;
-  }
+  protected taskForms = new FormGroup({
+    enterTitleTask: new FormControl('', [Validators.required]),
+    enterDescriptionTask: new FormControl('', [Validators.required])
+  })
 
 
   public onSaveTask(): void {
-    this.validationMessage = "";
-    const titleEmpty = this.enterTitleTask.trim().length === 0;
-    const descriptionEmpty = this.enterDescriptionTask.trim().length === 0;
-
-    if (titleEmpty || descriptionEmpty) {
-      if (titleEmpty && descriptionEmpty) {
-        this.validationMessage = "Veuillez remplir le titre et la description.";
-      } else if (titleEmpty) {
-        this.validationMessage = "Veuillez remplir le champ titre.";
-      } else if (descriptionEmpty) {
-        this.validationMessage = "Veuillez remplir le champ description.";
-      }
-      return;
-    }
-
     if(this.currentTask) {
-      this.currentTask.title = this.enterTitleTask;
-      this.currentTask.description = this.enterDescriptionTask;
+      this.currentTask.title = this.taskForms.controls.enterTitleTask.value;
+      this.currentTask.description = this.taskForms.controls.enterDescriptionTask.value;
       this.currentTask = null;
     }else{
       this.myTasks.push({
         id: Math.random(),
-        title: this.enterTitleTask,
-        description: this.enterDescriptionTask
+        title: this.taskForms.controls.enterTitleTask.value,
+        description: this.taskForms.controls.enterDescriptionTask.value
       });
     }
 
-    this.enterTitleTask = "";
-    this.enterDescriptionTask = "";
   }
 
   public onDeleteTask(id: number): void {
@@ -80,8 +53,8 @@ export class TaskForm {
     this.currentTask = task;
 
     if(this.currentTask){
-      this.enterTitleTask = task.title;
-      this.enterDescriptionTask = task.description;
+      this.taskForms.controls.enterTitleTask.patchValue(task.title);
+      this.taskForms.controls.enterDescriptionTask.patchValue(task.description);
     }
   }
 
--- src/app/types/task.type.ts
+++ src/app/types/task.type.ts
@@ -1,5 +1,5 @@
 export type TaskType = {
   id: number;
-  title: string;
-  description: string;
+  title: string | null;
+  description: string | null;
 };
